e268ee796c3a63085e7533dcb09018f3fc6f54f6
BryanLin <920616bryanlin@gmail.com>
2024-05-20 15:00:58 +0800

    test the url request to the model server

--- SitSmartDetection_SwiftUI/ProfileView.swift
+++ SitSmartDetection_SwiftUI/ProfileView.swift
@@ -31,9 +31,76 @@ struct ProfileView: View {
                 Image(.faRightFromBracket)
                     .foregroundColor(.accentColor)
             }
+//            Button(action: {
+//                guard let image = UIImage(named: "sample_img") else {
+//                    print("Image not found")
+//                    return
+//                }
+//                sendRequest(image: image)
+//            }, label: {
+//                Text("Button").padding()
+//            })
+            
         }
     }
 }
+
+//func sendRequest(image: UIImage) {
+//    guard let url = URL(string: "http://192.168.1.109:8000/predict_movenet") else {
+//        print("Invalid URL")
+//        return
+//    }
+//    
+//    var request = URLRequest(url: url)
+//    request.httpMethod = "POST"
+//    
+//    // 將圖片轉換為 JPEG Data
+//    guard let imageData = image.jpegData(compressionQuality: 0.8) else {
+//        print("Failed to convert image to data")
+//        return
+//    }
+//    
+//    // 設置請求頭
+//    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//    
+//    // 建立multipart/form-data的請求body
+//    let boundary = UUID().uuidString
+//    let contentType = "multipart/form-data; boundary=\(boundary)"
+//    request.setValue(contentType, forHTTPHeaderField: "Content-Type")
+//    
+//    var body = Data()
+//    let boundaryPrefix = "--\(boundary)\r\n"
+//    
+//    body.append(Data(boundaryPrefix.utf8))
+//    body.append(Data("Content-Disposition: form-data; name=\"file\"; filename=\"image.jpg\"\r\n".utf8))
+//    body.append(Data("Content-Type: image/jpeg\r\n\r\n".utf8))
+//    body.append(imageData)
+//    body.append(Data("\r\n".utf8))
+//    body.append(Data("--\(boundary)--\r\n".utf8))
+//    
+//    request.httpBody = body
+//    
+//    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+//        if let error = error {
+//            print("Error: \(error.localizedDescription)")
+//            return
+//        }
+//        
+//        guard let data = data else {
+//            print("No data received")
+//            return
+//        }
+//        
+//        if let predictionResult = String(data: data, encoding: .utf8) {
+//            print("Prediction result: \(predictionResult)")
+//        } else {
+//            print("Failed to decode response")
+//        }
+//    }
+//    
+//    task.resume()
+//}
+
 
 #Preview {
     ProfileView()
