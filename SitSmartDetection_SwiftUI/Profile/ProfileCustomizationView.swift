8b22bb161000386ee65ca6abff931bdc61b6c0d5
BryanLin <920616bryanlin@gmail.com>
2024-05-23 22:20:10 +0800

    ProfileView: finish profile setting layout

--- SitSmartDetection_SwiftUI/Profile/ProfileCustomizationView.swift
+++ SitSmartDetection_SwiftUI/Profile/ProfileCustomizationView.swift
@@ -6,13 +6,165 @@
 //
 
 import SwiftUI
+class CustomizationViewModel: ObservableObject {
+    @Published var postureAlertMinutes: Int = 2
+    @Published var postureAlertSeconds: Int = 30
+    @Published var idleAlertEnabled: Bool = true
+    @Published var goalPoints: Int = 85
+}
 
 struct ProfileCustomizationView: View {
+    @ObservedObject var viewModel = CustomizationViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Group {
+                HStack {
+                    Image(systemName: "figure.stand")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Text("Posture Alert")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                
+                Text("Time to alert when maintaining an incorrect posture.")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .padding(.horizontal)
+                
+//                Picker("Select Alert Type", selection: $viewModel.alertType) {
+//                    Text("Instant").tag("Instant")
+//                    Text("Delay").tag("Delay")
+//                }
+//                .pickerStyle(SegmentedPickerStyle())
+//                .background(Color(red: 151/255, green: 181/255, blue: 198/255))
+//                .cornerRadius(7)
+//                .padding()
+                
+                HStack {
+                    Picker(selection: $viewModel.postureAlertMinutes, label: Text("Minutes")) {
+                        ForEach(0..<60) { i in
+                            Text("\(i)").tag(i)
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    
+                    Text("min")
+                        .bold()
+                    
+                    Picker(selection: $viewModel.postureAlertSeconds, label: Text("Seconds")) {
+                        ForEach(0..<60) { i in
+                            Text("\(i)").tag(i)
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                    
+                    Text("sec")
+                        .bold()
+                }
+                .frame(height: 150)
+                
+                Divider()
+                    .padding()
+                
+                HStack {
+                    Image(systemName: "clock")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Text("Idle Alert")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                
+                Text("Time to alert when sitting over 20 minutes.")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .padding(.horizontal)
+                
+                CustomToggle(isOn: $viewModel.idleAlertEnabled)
+                    .padding()
+                
+                Divider()
+                    .padding()
+                
+                HStack {
+                    Image(systemName: "flag")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                    Text("Set Goal")
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                }
+                .padding()
+                
+                HStack {
+                    TextField("Goal Points", value: $viewModel.goalPoints, formatter: NumberFormatter())
+                        .keyboardType(.numberPad)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 100)
+                        .padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(10)
+                    
+                    Text("Points")
+                        .font(.body)
+                        .bold()
+                        .padding(.leading)
+                }
+            }
+            Spacer()
+        }
+        .navigationTitle("Customization")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    // Button action
+                }) {
+                    Text("Done").foregroundColor(.blue)
+                }
+            }
+        }
+        .padding()
     }
 }
 
-#Preview {
-    ProfileCustomizationView()
+struct CustomToggle: View {
+    @Binding var isOn: Bool
+    
+    var body: some View {
+        HStack {
+            Text("ON")
+                .foregroundColor(isOn ? .white : .gray)
+                .padding(.horizontal)
+                .background(isOn ? Color.blue : Color.clear)
+                .cornerRadius(15)
+            
+            Text("OFF")
+                .foregroundColor(!isOn ? .white : .gray)
+                .padding(.horizontal)
+                .background(!isOn ? Color.blue : Color.clear)
+                .cornerRadius(15)
+        }
+        .onTapGesture {
+            isOn.toggle()
+        }
+        .padding(5)
+        .background(Color.gray.opacity(0.2))
+        .cornerRadius(20)
+    }
 }
+
+struct ProfileCustomizationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            ProfileCustomizationView()
+        }
+    }
+}
+
--- SitSmartDetection_SwiftUI/Profile/ProfileSettingView.swift
+++ SitSmartDetection_SwiftUI/Profile/ProfileSettingView.swift
@@ -7,9 +7,113 @@
 
 import SwiftUI
 
+struct UserDataModel {
+    var name: String
+    var email: String
+}
+
+class SettingViewModel: ObservableObject {
+    @Published var userData: UserDataModel
+    
+    init() {
+        self.userData = UserDataModel(name: "Sunny", email: "t110590032@ntut.org.tw")
+    }
+}
+
 struct ProfileSettingView: View {
+    @State private var selectedGender: String = "Female"
+    @ObservedObject var viewModel = SettingViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("Avatar")
+                .resizable()
+                .frame(width: 65, height: 65)
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.accentColor, lineWidth: 3)
+                )
+            
+            Button(action: {
+                // Button action
+            }) {
+                Text("Edit Picture").foregroundColor(Color(red: 149/255, green: 208/255, blue: 248/255))
+            }
+            VStack(alignment: .leading, spacing: 20) {
+                HStack {
+                    Text("Name")
+                        .font(.headline)
+                        .foregroundColor(.deepAccent)
+                    Spacer()
+                    TextField("Name", text: $viewModel.userData.name)
+                        .multilineTextAlignment(.trailing)
+                        .foregroundColor(.profileAccent)
+                }
+                .padding(.horizontal)
+                
+                HStack(alignment: .top) {
+                    Text("Gender")
+                        .font(.headline)
+                        .foregroundColor(.deepAccent)
+                    Spacer()
+                    VStack(alignment: .leading, spacing: 10) {
+                        RadioButtonField(id: "Male", label: "Male", isMarked: $selectedGender)
+                        RadioButtonField(id: "Female", label: "Female", isMarked: $selectedGender)
+                        RadioButtonField(id: "Other", label: "Other", isMarked: $selectedGender)
+                    }
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Text("Email")
+                        .font(.headline)
+                        .foregroundColor(.deepAccent)
+                    Spacer()
+                    TextField("Email", text: $viewModel.userData.email)
+                        .multilineTextAlignment(.trailing)
+                        .foregroundColor(.profileAccent)
+                        .font(.body)
+                        .bold()
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+            }
+            .padding()
+            Spacer()
+            
+        }
+        .navigationTitle("Profile Settings")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    // Button action
+                }) {
+                    Text("Done").foregroundColor(.deepAccent)
+                }
+            }
+        }
+    }
+}
+
+struct RadioButtonField: View {
+    let id: String
+    let label: String
+    @Binding var isMarked: String
+    
+    var body: some View {
+        Button(action: {
+            self.isMarked = self.id
+        }) {
+            HStack {
+                Image(systemName: self.isMarked == self.id ? "largecircle.fill.circle" : "circle")
+                    .foregroundColor(self.isMarked == self.id ? Color(red: 0.2, green: 0.4, blue: 0.6) : .gray)
+                Text(label)
+                    .foregroundColor(.gray)
+                    .font(.body)
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
